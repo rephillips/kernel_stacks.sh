fa73961a567311f4fa9a402859c11963bd3847ff
rephillips <35229688+rephillips@users.noreply.github.com>
2022-06-23 18:03:10 -0700

    Update kstacks.sh

--- kstacks.sh
+++ kstacks.sh
@@ -5,7 +5,7 @@ while true
 do
 for i in `ps auxww | awk '{print $2,$8}' | grep D | grep -v PID | awk '{print $1}'` ; do
 if [ -n "$i" ]; then
-ps -fp ${i} >> $OUTPUT_DIR/kstacktrace.out ; cat /proc/${i}/stack >> $OUTPUT_DIR/ kstacktrace.out
+ps -fp ${i} >> $OUTPUT_DIR/kstacktrace.out ; cat /proc/${i}/stack >> $OUTPUT_DIR/kstacktrace.out
 fi
 done
 sleep $SAMPLE_PERIOD
