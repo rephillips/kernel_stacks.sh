b146c848b8493c6c3f9472e3e09ebe131c0e6d1b
rephillips <35229688+rephillips@users.noreply.github.com>
2022-11-02 18:23:13 -0700

    Create kstacks_all.sh

---
+++ kstacks_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+OUTPUT_DIR=/tmp/kstacks 
+SAMPLE_PERIOD=2
+while true
+do
+for i in `ps auxww | awk '{print $2,$8}' | grep -v PID | awk '{print $1}'` ; do
+if [ -n "$i" ]; then
+ps -fp ${i} >> $OUTPUT_DIR/kstacktrace.out ; cat /proc/${i}/stack >> $OUTPUT_DIR/kstacktrace.out
+fi
+done
+sleep $SAMPLE_PERIOD
+done
